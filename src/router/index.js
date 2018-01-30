f8143c14e20b18cefa89ffa340d85df5b5c794e4
CoderEnko007 <30767128+CoderEnko007@users.noreply.github.com>
2018-01-30 15:13:23 +0800

    [Title]: Install Element-UI Plugin [Date]: 2018/1/30 15:14 [Description]: Adapt Element-UI and test Navigation [Other]: N/A

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Blog from '@/views/Blog'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Blog',
+      component: Blog
     }
   ]
 })
---
+++ src/views/Blog.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-menu class="el-menu demo" mode="horizontal" v-bind:default-active="activeIndex" v-on:select="handleSelect">
+      <el-menu-item index="1">首页</el-menu-item>
+      <el-submenu index="2">
+          <template>分类a</template>
+          <el-menu-item index="2-1">JavaScript</el-menu-item>
+          <el-menu-item index="2-2">HTML/CSS</el-menu-item>
+          <el-menu-item index="2-3">Python</el-menu-item>
+          <el-menu-item index="2-4">Android</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="3">留言</el-menu-item>
+      <el-menu-item index="4">关于</el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: 'Blog',
+  data () {
+    return {
+      activeIndex = '1',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
